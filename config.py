6c7c351ea8dc65c343cdbcc7fb877b065d89c983
irzikistore <153968936+irzikistore@users.noreply.github.com>
2024-12-01 08:19:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-LOG_ID = int(getenv("LOG_ID", ""))
+
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 SUDO_USER = list(map(int, getenv("SUDO_USER", "").split()))
